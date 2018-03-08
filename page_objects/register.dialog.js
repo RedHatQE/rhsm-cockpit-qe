e8ea398be332dec58d769b255b931569f08be72e
Jan Stavěl @ RedHat <jstavel@redhat.com>
2018-03-08 18:58:01 +0100

    added proxy dialog usage

--- page_objects/register.dialog.js
+++ page_objects/register.dialog.js
@@ -1,29 +1,40 @@
 var Page = require('./Page');
-const env = require('env2')('.env');
-const url = require('url');
+const ProxyDialog = require('./proxy.dialog');
 
 var RegisterDialog = Object.create(Page, {
-    username: { get: function() { return browser.element('#subscription-register-username')}},
-    password: { get: function() { return browser.element('#subscription-register-password')}},
-    activationKey: { get: function() { return browser.element('#subscription-register-key')}},
-    useProxy:  { get: function() { return browser.element('#subscription-register-proxy-use')}},
-    mainFrame: { get: function() {
-        return browser.element('iframe[name="cockpit1:localhost/subscriptions"][data-loaded="1"]')
-    }},
-    registerButton: { get: function(){
-        return browser.element('div.modal-footer button.btn-primary'); 
-    }},
-    wait: { value: function() {
-        this.username.waitForVisible();
-        this.password.waitForVisible();
-        return this;
-    }},
-    registerWithUser: { value: function (username, password){
-        this.username.setValue(username);
-        this.password.setValue(password);
-        this.registerButton.click();
-        return this;
-    }}
+  username: { get: function() { return browser.element('#subscription-register-username');}},
+  password: { get: function() { return browser.element('#subscription-register-password');}},
+  org: { get: function() { return browser.element('#subscription-register-org');}},
+  activationKey: { get: function() { return browser.element('#subscription-register-key');}},
+  mainFrame: { get: function() {
+    return browser.element('iframe[name="cockpit1:localhost/subscriptions"][data-loaded="1"]');
+  }},
+  registerButton: { get: function(){
+    return browser.element('div.modal-footer button.btn-primary'); 
+  }},
+
+  wait: { value: function() {
+    this.username.waitForVisible();
+    this.password.waitForVisible();
+    return this;
+  }},
+  enableProxy: { value: function(){
+    ProxyDialog.useProxy.click();
+    return this;
+  }},
+  setNoAuthProxy: { value: function(location){
+    this.enableProxy();
+    ProxyDialog.setNoAuthProxy(location);
+    return this;
+  }},
+  registerWithUser: { value: function (username, password, orgid){
+    this.username.setValue(username);
+    this.password.setValue(password);
+    this.org.setValue(orgid);
+    browser.debug();
+    this.registerButton.click();
+    return this;
+  }}
 });
 
 module.exports = RegisterDialog;
