0ece4ebeb6db907ade48b8f8ecd978a387d94e6a
Jan Stavěl @ RedHat <jstavel@redhat.com>
2018-04-06 13:44:24 +0200

    added a subpage functions

--- page_objects/subscriptions.page.js
+++ page_objects/subscriptions.page.js
@@ -1,16 +1,26 @@
 var Page = require('./Page');
 const env = require('env2')('.env');
 const url = require('url');
+const UnregisterStatusElement = require('./unregistered-status.element.js');
+const RegisterDialog = require('./register.dialog.js');
 
 var SubscriptionsPage = Object.create(Page, {
-    mainFrame: { get: function() {
-        return browser.element('iframe[name="cockpit1:localhost/subscriptions"][data-loaded="1"]')
-    }},
-    wait: { value: function() {
-        this.mainFrame.waitForVisible();
-        browser.frame(this.mainFrame.value);
-        return this;
-    }},
+  mainFrame: { get: function() {
+    return browser.element('iframe[name="cockpit1:localhost/subscriptions"][data-loaded="1"]');
+  }},
+  wait: { value: function() {
+    this.mainFrame.waitForVisible();
+    browser.frame(this.mainFrame.value);
+    return this;
+  }},
+  atUnregisterStatus: { value: function(fn){
+    fn(UnregisterStatusElement);
+    return this;
+  }},
+  atRegisterDialog:{ value: function(fn){
+    fn(RegisterDialog);
+    return this;
+  }}
 });
 
 module.exports = SubscriptionsPage;
